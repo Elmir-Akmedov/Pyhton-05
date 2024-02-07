cbb0277d1920e059d745cfe46c5144fb8fe79a86
Elmir-Akmedov <156679110+Elmir-Akmedov@users.noreply.github.com>
2024-02-07 17:31:47 +0400

    .
    .

--- Homeworks-05/hw16.2.py
+++ Homeworks-05/hw16.2.py
@@ -150,8 +150,3 @@ in a 'for' loop?
     b) Use the enumerate() function
     c) Manually calculate the index
 """
-for num in range(10, 14):
-        for i in range(2, num):
-            if num % i == 1:
-               print(num)
-               break
